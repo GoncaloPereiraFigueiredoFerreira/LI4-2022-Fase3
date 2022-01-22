fe3725e4f41235dd91ecce4ee5760250c33bb9fb
Luis Silva <56602110+LuisMPSilva01@users.noreply.github.com>
2022-01-22 20:48:14 +0000

    ez

---
+++ SpotGuru/SpotGuru/Controllers/HorariosController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SpotGuru.Data;
+using SpotGuru.Models;
+using SpotGuru.ViewModels;
+
+namespace SpotGuru.Controllers
+{
+    [Authorize]
+    public class HorariosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public HorariosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Horarios/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var horario = await _context.Horario.Include("Slots")
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (horario == null)
+            {
+                return NotFound();
+            }
+
+            return View(horario);
+        }
+    }
+}
